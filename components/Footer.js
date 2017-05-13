1909007eeaefbdd7f828e5d8bbbc58b49139c2cd
SIVADASS-DESKTO\Sivadass <nsivadass@gmail.com>
2017-05-14 00:04:16 +0530

    Minified build is added

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,6 @@ const Footer = (props) =>{
     return(
         <footer>
             <p>&copy; 2017 <strong>Veggy</strong> - Organic Green Store</p>
-            <p><a className="github-button" href="https://github.com/sivadass/react-shopping-cart/fork" data-icon="octicon-repo-forked" data-size="large" data-show-count="true" aria-label="Fork sivadass/react-shopping-cart on GitHub">Fork it</a></p>
         </footer>
     )
 };
