e8cbde0c717eeadf8e0231a7da0443cf9ef6d7cd
Sivadass N <nsivadass@gmail.com>
2021-04-06 09:05:33 +0530

    Checkout in progress

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -1,12 +1,77 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import classNames from "classnames";
+import {
+  CartStateContext,
+  CartDispatchContext,
+  removeFromCart,
+  toggleCartPopup
+} from "contexts/cart";
 
 const Checkout = () => {
+  const { items } = useContext(CartStateContext);
   return (
-    <div>
-      <h1>Checkout Page</h1>
-      <div>Contents</div>
+    <div className="checkout-page">
+      <div className="container">
+        <div className="order-details">
+          <ul className="timeline">
+            <li className="done">
+              <h2>Shipping</h2>
+            </li>
+            <li className="active">
+              <h2>Payment</h2>
+            </li>
+            <li>
+              <h2>Review</h2>
+            </li>
+          </ul>
+        </div>
+        <div className="order-summary">
+          <h2>Summary</h2>
+          <ul className="cart-items">
+            {items.map((product) => {
+              return (
+                <li className="cart-item" key={product.name}>
+                  <img className="product-image" src={product.image} />
+                  <div className="product-info">
+                    <p className="product-name">{product.name}</p>
+                    <p className="product-price">{product.price}</p>
+                  </div>
+                  <div className="product-total">
+                    <p className="quantity">
+                      {`${product.quantity} ${
+                        product.quantity > 1 ? "Nos." : "No."
+                      }`}
+                    </p>
+                    <p className="amount">{product.quantity * product.price}</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+
+          <ul className="total-breakup">
+            <li>
+              <p>Subtotal</p>
+              <p>5000</p>
+            </li>
+            <li>
+              <p>Tax</p>
+              <p>5000</p>
+            </li>
+            <li>
+              <p>Shipping</p>
+              <p>5000</p>
+            </li>
+            <li>
+              <h2>Total</h2>
+              <h2>5000</h2>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Checkout;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import ProductCard from "components/Product";
 import {
   ProductsStateContext,
